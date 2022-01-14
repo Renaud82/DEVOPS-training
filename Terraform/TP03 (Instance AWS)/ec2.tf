bd89564f69adbd160df6e669c4fdb6e76a55320f
Renaud82 <95615714+Renaud82@users.noreply.github.com>
2022-01-14 09:09:27 +0100

    Update ec2.tf

--- Terraform/TP03 (Instance AWS)/ec2.tf
+++ Terraform/TP03 (Instance AWS)/ec2.tf
@@ -5,13 +5,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "renaud-ec2" {
-    ami = "ami-04505e74c0741db8d"
+    ami           = "ami-04505e74c0741db8d"
     instance_type = "t2.micro"
-    key_name = "renaud-kp-ajc"
-    tags = {
-        Name = "renaud-ec2-terraform"
+    key_name      = "renaud-kp-ajc"
+    tags          = {
+        Name      = "renaud-ec2-terraform"
         formation = "Frazer"
-        iac = "terraform"
+        iac       = "terraform"
     }
 }
 

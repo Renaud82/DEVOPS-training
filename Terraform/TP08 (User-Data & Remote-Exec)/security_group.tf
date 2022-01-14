d859c49c74ba6fcea04851c1104e9997a9720504
Renaud82 <95615714+Renaud82@users.noreply.github.com>
2022-01-14 09:14:47 +0100

    Update security_group.tf

--- Terraform/TP08 (User-Data & Remote-Exec)/security_group.tf
+++ Terraform/TP08 (User-Data & Remote-Exec)/security_group.tf
@@ -21,10 +21,10 @@ resource "aws_security_group" "sg-nginx" {
   }
 
   egress {
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  from_port        = 0
+  to_port          = 0
+  protocol         = "-1"
+  cidr_blocks      = ["0.0.0.0/0"]
   ipv6_cidr_blocks = ["::/0"]
   }
 

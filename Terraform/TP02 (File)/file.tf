2d889ac7b388fb2362a10b3c7ac7f983e9319033
Renaud82 <95615714+Renaud82@users.noreply.github.com>
2022-01-14 09:11:38 +0100

    Update file.tf

--- Terraform/TP02 (File)/file.tf
+++ Terraform/TP02 (File)/file.tf
@@ -1,4 +1,4 @@
 resource "local_file" "file1" {
-    filename="/Users/renaudsautour/Downloads/DEVOPS/Terraform/TP2/Renaud.txt"
-    content="Bonjour Renaud"
+    filename = "/Users/renaudsautour/Downloads/DEVOPS/Terraform/TP2/Renaud.txt"
+    content  = "Bonjour Renaud"
 }

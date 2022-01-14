b2e5706d9728207e44c6398462f9fdca246dacac
Renaud82 <95615714+Renaud82@users.noreply.github.com>
2022-01-14 09:08:43 +0100

    Update main.tf

--- Terraform/TP10 (Modules)/prod/main.tf
+++ Terraform/TP10 (Modules)/prod/main.tf
@@ -1,10 +1,10 @@
 module "ec2_prod" {
-    source = "../modules"
+    source        = "../modules"
     instance_type = "t2.micro"
-    ami = "ami-04505e74c0741db8d"
+    ami           = "ami-04505e74c0741db8d"
     key_name      = "renaud-kp-ajc"
-    tag_name = "renaud-ec2-prod"
+    tag_name      = "renaud-ec2-prod"
     tag_formation = "Frazer"
-    tag_iac = "terraform"
-    environment = "prod"
+    tag_iac       = "terraform"
+    environment   = "prod"
 }
